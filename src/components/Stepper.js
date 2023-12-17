53538dc685204da0e4fb98577c07642c42d1ee53
yoyo <a4253607@gmail.com>
2023-12-17 19:14:01 +0800

    add step

---
+++ src/components/Stepper.js
@@ -0,0 +1,20 @@
+import '../style/helpers/_stepper.scss'
+
+function Stepper({ activeStep, steps }) {
+    return (
+        <>
+            <ol className="stepper p-3">
+                {steps.map((step,i) => {
+                    return (
+                        <li className="stepper_item " key={i}>
+                            <h3 className={`stepper_title mb-3 ${activeStep === i?'active':'' }`}>{i+1}</h3>
+                            <p className="stepper_desc mb-0">{step.title}</p>
+                        </li>
+                    )
+                })}
+            </ol>
+        </>
+    )
+}
+
+export default Stepper
