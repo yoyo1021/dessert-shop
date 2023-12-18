53a573eff868c4164482acd0fe866ddb7c08fe53
yoyo <a4253607@gmail.com>
2023-12-18 16:17:22 +0800

    新增複製貼上功能

--- src/pages/front/Home.js
+++ src/pages/front/Home.js
@@ -45,7 +45,8 @@ function Home() {
     }
 
     const copyCoupon = ()=>{
-        //console.log(copyRef.current.innerText)
+        //console.log(copyRef.current.innerText);
+        navigator.clipboard.writeText(copyRef.current.innerText)
         setIsCopy(true);
         setTimeout(()=>{
             setIsCopy(false);
